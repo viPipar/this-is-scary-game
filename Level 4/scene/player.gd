a6f9aa34dd02fb80a74d16198d41b1b7565d06c9
RaftFeed <rafidharsyahh@gmail.com>
2026-02-06 22:57:06 +0700

    Level 4

--- Level 4/scene/player.gd
+++ Level 4/scene/player.gd
@@ -5,29 +5,6 @@ const SPEED = 400.0
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2.ZERO
-	
-	if direction.x > 0:
-		anim.play("walk")
-		anim.flip_h = false
-	if direction.x < 0:
-		anim.play("walk")
-		anim.flip_h = true
-		
-	# Input WASD
-	if Input.is_action_pressed("right"):
-		direction.x += 1
-		anim.play("walk")
-		anim.flip_h = false
-	if Input.is_action_pressed("left"):
-		direction.x -= 1
-		anim.play("walk")
-		anim.flip_h = true
-	if Input.is_action_pressed("back"):
-		direction.y += 1
-		anim.play("walk")
-	if Input.is_action_pressed("forward"):
-		direction.y -= 1
-		anim.play("walk")
 
 	# Biar kecepatan diagonal tetap konsisten
 	if direction != Vector2.ZERO:
