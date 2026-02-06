8f26a50dec669e235979634d658f7d1b990281c9
viPipar <rafifilmanyy@gmail.com>
2026-02-06 22:09:46 +0700

    Level 1 Fail and Win Condition Fixed

--- Level_1_Banana/level_1_banana.gd
+++ Level_1_Banana/level_1_banana.gd
@@ -10,3 +10,7 @@ func _on_finish_body_entered(body: Node2D) -> void:
 	if body.name == "Father":
 		await get_tree().create_timer(2.0).timeout
 		print("Father menang")
+		$CanvasLayer.visible = true
+		$CanvasLayer/ColorRect/AnimationPlayer.play("restart_game")
+		await get_tree().create_timer(1.0).timeout
+		get_tree().change_scene_to_file("res://Level 2/scene/Level 2.tscn")
--- Level_5_Crossy/finish.gd
+++ Level_5_Crossy/finish.gd
@@ -1,4 +1,5 @@
 extends Area2D
+@onready var car_object: Sprite2D = $"../Car"
 
 # Pastikan sinyal body_entered sudah terhubung ke fungsi ini
 func _on_body_entered(body: Node2D) -> void:
@@ -8,5 +9,13 @@ func _on_body_entered(body: Node2D) -> void:
 		print("Finish tersentuh!")
 		
 		# Cek apakah script Daughter punya fungsi 'freeze_character'
+	# Cek apakah kita sudah memasukkan node Car di Inspector?
+		if car_object != null:
+			# Panggil fungsi 'mulai_jalan' yang ada di script Car
+			if car_object.has_method("mulai_jalan"):
+				car_object.mulai_jalan()
+		else:
+			print("LUPA MEMASUKKAN NODE CAR DI INSPECTOR!")
+		
 		if body.has_method("freeze_character"):
 			body.freeze_character() # <--- Panggil fungsi di script Daughter tadi
