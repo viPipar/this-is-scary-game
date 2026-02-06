690494e5c7a4a0cad95b23251bf09ee5a63ba450
RaftFeed <rafidharsyahh@gmail.com>
2026-02-06 23:37:38 +0700

    Final Banget Ini Mah Aseli WOk

--- Level 4/scene/sprite_2d.gd
+++ Level 4/scene/sprite_2d.gd
@@ -1,11 +1,27 @@
 extends Sprite2D
 
+var tween: Tween
+var moved := false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Pause game saat mulai
+	get_tree().paused = true
+	# Supaya Sprite2D ini tetap bisa menerima input walau game pause
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if moved:
+		return
+		
+	# Klik untuk mulai game + gerakkan sprite
+	if Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		get_tree().paused = false   # resume game
+		move_down()
+
+func move_down():
+	moved = true
+	
+	tween = create_tween()
+	tween.tween_property(self, "position:y", position.y + 5000, 2.0)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
